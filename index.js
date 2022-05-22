e3156d99c404a405425e99f9e03bb70718e9037d
Aron Galvan <arongalvan03@gmail.com>
2022-05-22 12:18:31 -0700

    commit tree 61d576d9a495468be075763cccc926b53410707f

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+    const frstTwoDrivers = [];
+    frstTwoDrivers.unshift(drivers[1]);
+    frstTwoDrivers.unshift(drivers[0]);
+    return frstTwoDrivers;
+}
+
+const returnLastTwoDrivers = (drivers) => {
+    const lastTwoDrivers = [];
+    lastTwoDrivers.unshift(drivers[drivers.length - 1]);
+    lastTwoDrivers.unshift(drivers[drivers.length - 2]);
+    return lastTwoDrivers;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (multiplier) => {
+    return fare => fare * multiplier;
+}
+
+const fareDoubler = fare => {
+    const fareMutiplier = createFareMultiplier(2);
+    return fareMutiplier(fare);
+};
+
+const fareTripler = fare => {
+    const fareMutiplier = createFareMultiplier(3);
+    return fareMutiplier(fare);
+};
+
+const fareQuintupler = fare => {
+    const fareMutiplier = createFareMultiplier(4);
+    return fareMutiplier(fare);
+};
+
+
+const selectDifferentDrivers = (arrayOfDrivers, functionSelectDrivers) => {
+    return functionSelectDrivers(arrayOfDrivers);
+}
